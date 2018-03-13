86a5197e56dcfc1679c2756041cb15cfb41dfb3a
刘炳文 <liubingwen@huianrong.com>
2018-03-13 09:32:05 +0800

    修改测试模拟器

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -11,8 +11,8 @@ module.exports = function (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['Chrome'],
-    frameworks: ['mocha', 'sinon-chai'],
+    browsers: ['PhantomJS'],
+    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
